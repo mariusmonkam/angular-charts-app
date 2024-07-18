4d63746a3d41026647031c1ba21a239295269dd6
MariusMonkam <cloudmonde@yahoo.com>
2024-07-18 13:13:51 +0300

    generate chart-list and chart-settings component

---
+++ src/app/components/chart-list/chart-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './chart-list.component.html',
+  styleUrl: './chart-list.component.css'
+})
+export class ChartListComponent {
+
+}
