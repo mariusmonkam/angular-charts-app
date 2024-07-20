c622a6c9e256c9c14dcc6a6d7f775ef7bd4527e2
MariusMonkam <cloudmonde@yahoo.com>
2024-07-20 23:06:12 +0300

    fix : date range filter fix

--- src/app/components/date-range-filter/date-range-filter.component.ts
+++ src/app/components/date-range-filter/date-range-filter.component.ts
@@ -1,8 +1,12 @@
-// src/app/components/date-range-filter/date-range-filter.component.ts
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { Store } from '@ngrx/store';
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { updateDateRange } from '../../store/chart/chart.actions';
+import { Store } from '@ngrx/store';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -24,26 +28,38 @@ import * as ChartActions from '../../store/chart/chart.actions';
     MatButtonModule,
   ],
 })
-export class DateRangeFilterComponent implements OnInit {
+export class DateRangeFilterComponent implements OnInit, OnDestroy {
   dateRangeForm: FormGroup;
+  private resizeSubscription: any;
+
   @Output() dateRangeChanged = new EventEmitter<{
-    startDate: Date;
-    endDate: Date;
+    startDate: string | null;
+    endDate: string | null;
   }>();
 
   constructor(private fb: FormBuilder, private store: Store) {
     this.dateRangeForm = this.fb.group({
-      startDate: [''],
-      endDate: [''],
+      startDate: [null],
+      endDate: [null],
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    // Initialize any additional setup or subscriptions if necessary
+  }
+
+  ngOnDestroy() {
+    // Cleanup logic if needed
+    // For example, if there are any subscriptions or event listeners, unsubscribe or remove them here.
+    if (this.resizeSubscription) {
+      this.resizeSubscription.unsubscribe();
+    }
+  }
 
   onSubmit() {
     if (this.dateRangeForm.valid) {
       const { startDate, endDate } = this.dateRangeForm.value;
-      this.store.dispatch(updateDateRange({ startDate, endDate }));
+      this.store.dispatch(ChartActions.updateDateRange({ startDate, endDate }));
       this.dateRangeChanged.emit({ startDate, endDate });
     }
   }
